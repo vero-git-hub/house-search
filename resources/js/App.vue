fd30c1b66bbd849ee05e0db5dab0bf1575ebe7c2
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-07-28 15:22:16 +0300

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>Hello from Vue.js!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+/* Ваши стили здесь */
+</style>
